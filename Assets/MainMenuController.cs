d75476aeab9da903f0bc9f236396368cdd1485a7
lynaFEGAS <fegaslyna@gmail.com>
2025-12-29 00:42:43 +0100

    Ajout du Menu Principal avec 3 boutons fonctionnels
    - Titre "Mario à l'USTHB" en jaune
    - Bouton Play (vert) qui charge Level1
    - Bouton Settings (bleu)
    - Bouton Quit (rouge) qui ferme le jeu
    - Script MainMenuController créé

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1_Bibliotheque");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings button clicked!");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
+
